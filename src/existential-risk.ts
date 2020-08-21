82ad499b7cd7872a5413a059d1fad99a087c7391
Esa Koskinen <esakoskin2@gmail.com>
2020-08-22 03:30:41 +0900

    make a more impressive demo with the canvas

--- src/existential-risk.ts
+++ src/existential-risk.ts
@@ -5,3 +5,20 @@ const ctx = (document.getElementById(
 ctx.beginPath()
 ctx.rect(20, 20, 150, 100)
 ctx.stroke()
+
+let i = 0
+const interval = setInterval(() => {
+  for (let x = i; x < 1400; x += 5) {
+    for (let y = i; y < 900; y += 5) {
+      ctx.beginPath()
+      ctx.strokeStyle = `rgb(
+        0,
+        ${Math.floor(255 - x / 5)},
+        ${Math.floor(255 - x / 4)})`
+      ctx.arc(x, y, 3, 0, 2 * Math.PI)
+      ctx.stroke()
+    }
+  }
+  i++
+  i === 8 && clearInterval(interval)
+}, 200)
