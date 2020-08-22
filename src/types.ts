65c545be9ee8e7f3a158f203100f604b6e1cd673
Esa Koskinen <esakoskin2@gmail.com>
2020-08-23 00:10:08 +0900

    add a TON of hard-work-research into the continent data
    - also updated types and added the sources

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,9 @@
 export type GameState = {
   day: number
   continentSections: ContinentSection[]
+  co2ppm: number // co2 parts per million in atmosphere, based on extrapolating https://www.kaggle.com/ucsandiego/carbon-dioxide
+  globalTempDiff: number // change in global surface temperature relative to 1951-1980 average temperatures, https://climate.nasa.gov/vital-signs/global-temperature/
+  globalTempDiffDelta: number // yearly change in globalTempDiff; high temp causes unrest and finances to go down, causing emigration
 }
 
 export type ContinentName =
@@ -21,6 +24,37 @@ export type ContinentName =
 export type ContinentSection = {
   name: ContinentName
   totalPopulation: number
+  birthRate: number // births / 1000 pop / year
+  birthRateDelta: number // acceleration of population growth; decreases from high education or low happiness
+  lifeExpectancy: number // years; this, disease, and conflict causes daily deaths. Can increase from high tech/finance
+  lifeExpectancyDelta: number // acceleration of life expectancy; increased by tech improvements
+
+  happiness: number // float from 0 to 10; indicates optimism and generosity. Low happiness compared to neighbors causes immigration
+  happinessGrowth: number // Acceleration of happiness; influenced by conflict, finance, education, and tech
+
+  financeIndex: number // 0 to 10, level of financial freedom. Influences happiness and causes education index to grow/decrease.
+  educationIndex: number // 0 to 10, level of education. Influences tech and finance.
+  //                        Free universal junior high corresponds to 5; upper sec. to 7, uni to 9, doctorate to 10
+  techIndex: number // 0 to 100, level of tech in the region. 8 to 10 is normal for US 2000s, 10-25 normal for US 2010s, anything beyond causes unrest and AI/nuclear threat
+  techIndexDelta: number // acceleration of tech index
+
+  diseaseIndex: number // 0 to 100, normally around 0-5. Causes deaths. Education decreases, random events cause a jump in proportion to finance index.
+
+  /**
+   * 0 is absolute peace,
+   * 1 is minor unrest and occasional violence,
+   * 2 is general unrest and local conflict,
+   * 3 is major unrest and national conflict,
+   * 4 is civil war or borderless conflict between independent groups
+   * 5 is international war within a region
+   * 6 is international war within
+   * 7 is international war between continents or religions
+   * 8 is large-scale international war
+   * 9 is nuclear war between two or more countries
+   */
+  conflictLevel: number // float with level of conflict within the region; two parties may not have the same level (usually winning belligerent has less unrest)
+
+  subRegions: Partial<ContinentSection[]> // unused
   neighbors: ContinentName[] // names of neighboring continents
   xywh: Rectangle // position of map rectangle. x-coordinate, y-coordinate, width, height
 }
@@ -43,65 +77,209 @@ export type MouseBuffer = {
 export const initialGameState = (): GameState => ({
   day: 0, // From 2020-01-01
   continentSections: initialContinents(),
+  co2ppm: 415.5, // co2 parts per million in atmosphere, based on extrapolating https://www.kaggle.com/ucsandiego/carbon-dioxide
+  globalTempDiff: 0.99, // change in global surface temperature relative to 1951-1980 average temperatures, https://climate.nasa.gov/vital-signs/global-temperature/
+  globalTempDiffDelta: 0.039, // yearly change in globalTempDiff; high temp causes unrest and finances to go down, causing emigration
 })
 
-// Based on 2020 data from https://www.worldometers.info/geography/7-continents/
+// Population counts based on 2020 data from: https://www.worldometers.info/geography/7-continents/
+// Population growth and delta based on https://ourworldindata.org/grapher/birth-rate-vs-death-rate?stackMode=absolute&time=2006..latest
+// Happiness based on World Happiness Report: https://www.kaggle.com/londeen/world-happiness-report-2020, growth on historical data from https://www.kaggle.com/unsdsn/world-happiness
+// Life expectancy based on Our World in Data report: https://ourworldindata.org/life-expectancy
+// Conflict level based on guesstimations and news
 const initialContinents = (): ContinentSection[] => [
   {
     name: 'Africa',
     totalPopulation: 1340598147,
+    birthRate: 35.91,
+    birthRateDelta: -0.41,
+    lifeExpectancy: 63.2,
+    lifeExpectancyDelta: 0.46,
+    happiness: 4.571100235,
+    happinessGrowth: 0.0304200469,
+    financeIndex: 0,
+    educationIndex: 0,
+    techIndex: 0,
+    techIndexDelta: 0,
+    diseaseIndex: 0,
+    conflictLevel: 0,
+    subRegions: [],
     neighbors: ['Europe', 'Asia', 'South America', 'Antarctica'],
     xywh: [590, 380, 210, 280],
   },
   {
     name: 'Asia',
     totalPopulation: 4641054775 - 144386830, // Subtract Russia as it became its own continent
+    birthRate: 21.2,
+    birthRateDelta: -0.386,
+    lifeExpectancy: 73.6,
+    lifeExpectancyDelta: 0.27,
+    happiness: 5.384300232, // Median from 2020. 2015 data medians: Southeast: 5.353499889, South: 4.75415659, East: 5.789672375
+    happinessGrowth: 0.017038122,
+    financeIndex: 0,
+    educationIndex: 0,
+    techIndex: 0,
+    techIndexDelta: 0,
+    diseaseIndex: 0,
+    conflictLevel: 0,
+    subRegions: [],
     neighbors: ['Africa', 'Australia', 'Europe', 'North America', 'Russia'],
     xywh: [800, 315, 360, 245],
   },
   {
     name: 'Europe',
     totalPopulation: 747636026,
+    birthRate: 9.9,
+    birthRateDelta: -0.152,
+    lifeExpectancy: 78.6,
+    lifeExpectancyDelta: 0.23,
+    happiness: 6.5845398902, // Average from 2020 medians: Northern: 7.504499912, Central: 6.215499878, Eastern: 5.949999809, Southern: 6.15899992, Western: 7.093699932,
+    happinessGrowth: 0.106840016,
+    financeIndex: 0,
+    educationIndex: 0,
+    techIndex: 0,
+    techIndexDelta: 0,
+    diseaseIndex: 0,
+    conflictLevel: 0,
+    subRegions: [],
     neighbors: ['Africa', 'Asia', 'North America', 'Russia'],
     xywh: [600, 200, 170, 180],
   },
   {
     name: 'North America',
     totalPopulation: 592072212,
+    birthRate: 12.8,
+    birthRateDelta: -0.24,
+    lifeExpectancy: 0,
+    lifeExpectancyDelta: 0,
+    happiness: 0,
+    happinessGrowth: 0,
+    financeIndex: 0,
+    educationIndex: 0,
+    techIndex: 0,
+    techIndexDelta: 0,
+    diseaseIndex: 0,
+    conflictLevel: 0,
+    subRegions: [],
     neighbors: ['Asia', 'Europe', 'Central America', 'Russia'],
     xywh: [130, 150, 380, 250],
   },
   {
     name: 'Central America',
     totalPopulation: 430759766,
+    birthRate: 20.97,
+    birthRateDelta: -0.32,
+    lifeExpectancy: 0,
+    lifeExpectancyDelta: 0,
+    happiness: 0,
+    happinessGrowth: 0,
+    financeIndex: 0,
+    educationIndex: 0,
+    techIndex: 0,
+    techIndexDelta: 0,
+    diseaseIndex: 0,
+    conflictLevel: 0,
+    subRegions: [],
     neighbors: ['North America', 'South America'],
     xywh: [200, 400, 200, 90],
   },
   {
     name: 'South America',
     totalPopulation: 430759766,
+    birthRate: 15.2,
+    birthRateDelta: -0.261,
+    lifeExpectancy: 0,
+    lifeExpectancyDelta: 0,
+    happiness: 0,
+    happinessGrowth: 0,
+    financeIndex: 0,
+    educationIndex: 0,
+    techIndex: 0,
+    techIndexDelta: 0,
+    diseaseIndex: 0,
+    conflictLevel: 0,
+    subRegions: [],
     neighbors: ['Africa', 'Central America', 'Antarctica'],
     xywh: [350, 490, 170, 270],
   },
   {
     name: 'Antarctica',
     totalPopulation: 2687,
+    birthRate: 2.3,
+    birthRateDelta: -0.18,
+    lifeExpectancy: 71.7,
+    lifeExpectancyDelta: 0.13,
+    happiness: 0,
+    happinessGrowth: 0,
+    financeIndex: 0,
+    educationIndex: 0,
+    techIndex: 0,
+    techIndexDelta: 0,
+    diseaseIndex: 0,
+    conflictLevel: 0,
+    subRegions: [],
     neighbors: ['Africa', 'South America'],
     xywh: [100, 800, 1180, 97],
   },
   {
     name: 'Australia',
     totalPopulation: 42677813,
+    birthRate: 14.5,
+    birthRateDelta: -0.24,
+    lifeExpectancy: 0,
+    lifeExpectancyDelta: 0,
+    happiness: 0,
+    happinessGrowth: 0,
+    financeIndex: 0,
+    educationIndex: 0,
+    techIndex: 0,
+    techIndexDelta: 0,
+    diseaseIndex: 0,
+    conflictLevel: 0,
+    subRegions: [],
     neighbors: ['Asia'],
     xywh: [1050, 560, 230, 150],
   },
   {
     name: 'Russia',
     totalPopulation: 144386830,
+    birthRate: 12.9,
+    birthRateDelta: 0.16,
+    lifeExpectancy: 0,
+    lifeExpectancyDelta: 0,
+    happiness: 0,
+    happinessGrowth: 0,
+    financeIndex: 0,
+    educationIndex: 0,
+    techIndex: 0,
+    techIndexDelta: 0,
+    diseaseIndex: 0,
+    conflictLevel: 0,
+    subRegions: [],
     neighbors: ['Asia', 'Europe', 'North America'],
     xywh: [770, 170, 550, 145],
   },
 ]
+
+const continentBase: ContinentSection = {
+  name: 'Europe',
+  totalPopulation: 0,
+  birthRate: 0,
+  birthRateDelta: 0,
+  lifeExpectancy: 0,
+  lifeExpectancyDelta: 0,
+  happiness: 0,
+  happinessGrowth: 0,
+  financeIndex: 0,
+  educationIndex: 0,
+  techIndex: 0,
+  techIndexDelta: 0,
+  diseaseIndex: 0,
+  conflictLevel: 0,
+  subRegions: [],
+  neighbors: [],
+  xywh: [0, 0, 200, 200],
+}
 
 export const initialMouseBuffer = (): MouseBuffer => ({
   lastMouseX: 0,
