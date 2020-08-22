5beb9f534c60b6d9412568e8867493f096871ea4
Esa Koskinen <esakoskin2@gmail.com>
2020-08-23 02:01:26 +0900

    more data - add food index and GDP stats

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@
 export type GameState = {
   day: number
   continentSections: ContinentSection[]
-  co2ppm: number // co2 parts per million in atmosphere. Values above 300 slightly hamper education. Based on extrapolating https://www.kaggle.com/ucsandiego/carbon-dioxide
+  co2ppm: number // co2 parts per million in atmosphere. Values above 300 slightly hamper education. Values above 400 are harmful; outdoor air pollution is the world's 4th numerous killer with 4.9 deaths caused yearly. High values may boost crop productivity, though. Based on extrapolating https://www.kaggle.com/ucsandiego/carbon-dioxide
   globalTempDiff: number // change in global surface temperature relative to 1951-1980 average temperatures, https://climate.nasa.gov/vital-signs/global-temperature/
   globalTempDiffDelta: number // yearly change in globalTempDiff; high temp causes unrest and decreases finances, causing emigration
 }
@@ -28,12 +28,14 @@ export type ContinentSection = {
   birthRateDelta: number // acceleration of population growth yer year; decreases from high education or low happiness
   lifeExpectancy: number // years; this, disease, and conflict causes daily deaths. Can increase from high tech/finance
   lifeExpectancyDelta: number // acceleration of life expectancy per year; increased by tech improvements
+  GDPCapita: number // GDP per capita as of 2019, in international PPP adjusted dollars
+  GDPCapitaMultiplier: number // annual multiplier for GDP in percent (e.g. 1.03 for 3% increase, 0.84 for 16% decrease)
 
   happiness: number // float from 0 to 10; indicates optimism and generosity. Low happiness compared to neighbors causes immigration
   happinessDelta: number // Acceleration of happiness per year; influenced by conflict, finance, education, and tech
 
-  foodIndex: number // 0 to 10, higher is better. Level of malnourishment and famine. Finance increases but mass immigration and conflict greatly decreases. High values boost education.
-  financeIndex: number // 0 to 10, level of financial freedom. Influences happiness and causes education index to grow/decrease.
+  foodIndex: number // 0 to 10, higher is better. Level of malnourishment and famine. Finance increases but mass immigration and conflict greatly decreases. High values boost education. Anything less than 5 is extremely alarming; 5-8 is serious and more than 9 normal.
+  financeIndex: number // 0 to 10, level of financial freedom. Influences happiness and causes education index to grow/decrease. Exponential. 8 equals a 50k PPP$ GDP/pc
   educationIndex: number // 0 to 10, level of education. Influences tech and finance.
   //                        Free universal junior high corresponds to 5; upper sec. to 7, uni to 9, doctorate to 10
   techIndex: number // 0 to 100, level of tech in the region. 8 to 10 is normal for US 2000s, 10-25 normal for US 2010s, anything beyond causes unrest and AI/nuclear threat
@@ -94,8 +96,9 @@ export const initialGameState = (): GameState => ({
 // Population growth and delta based on https://ourworldindata.org/grapher/birth-rate-vs-death-rate?stackMode=absolute&time=2006..latest
 // Happiness based on World Happiness Report: https://www.kaggle.com/londeen/world-happiness-report-2020, growth on historical data from https://www.kaggle.com/unsdsn/world-happiness
 // Life expectancy based on Our World in Data report: https://ourworldindata.org/life-expectancy
+// GDP is based on International Monetary Fund's data: https://www.imf.org/external/datamapper/NGDP_RPCH@WEO/OEMDC/ADVEC/WEOWORLD
 // Food index is based on the inverse of Global Hunger Index, from: https://ourworldindata.org/hunger-and-undernourishment
-// Finance index is based on median percentile of GDP, compiled from data on Our World in Data
+// Finance index is based on median percentile of GDP, compiled from data on Our World in Data.
 // globalTempDiffSensitivity based on estimations from World Climate Map: https://www.mapsofworld.com/world-maps/world-climate-map.html
 // Conflict level based on guesstimations and news
 const initialContinents = (): ContinentSection[] => [
@@ -106,9 +109,11 @@ const initialContinents = (): ContinentSection[] => [
     birthRateDelta: -0.41,
     lifeExpectancy: 63.2,
     lifeExpectancyDelta: 0.46,
+    GDPCapita: 1930,
+    GDPCapitaMultiplier: 1.036,
     happiness: 4.571100235,
     happinessDelta: 0.0304200469,
-    foodIndex: 0,
+    foodIndex: 7.1,
     financeIndex: 0,
     educationIndex: 0,
     techIndex: 0,
@@ -127,6 +132,8 @@ const initialContinents = (): ContinentSection[] => [
     birthRateDelta: -0.386,
     lifeExpectancy: 73.6,
     lifeExpectancyDelta: 0.27,
+    GDPCapita: 7350,
+    GDPCapitaMultiplier: 1.061,
     happiness: 5.384300232, // Median from 2020. 2015 data medians: Southeast: 5.353499889, South: 4.75415659, East: 5.789672375
     happinessDelta: 0.017038122,
     foodIndex: 8.4,
@@ -148,9 +155,11 @@ const initialContinents = (): ContinentSection[] => [
     birthRateDelta: -0.152,
     lifeExpectancy: 78.6,
     lifeExpectancyDelta: 0.23,
+    GDPCapita: 29410,
+    GDPCapitaMultiplier: 1.027,
     happiness: 6.5845398902, // Average from 2020 medians: Northern: 7.504499912, Central: 6.215499878, Eastern: 5.949999809, Southern: 6.15899992, Western: 7.093699932,
     happinessDelta: 0.076499975,
-    foodIndex: 0,
+    foodIndex: 9.7,
     financeIndex: 0,
     educationIndex: 0,
     techIndex: 0,
@@ -169,9 +178,11 @@ const initialContinents = (): ContinentSection[] => [
     birthRateDelta: -0.24,
     lifeExpectancy: 79.2,
     lifeExpectancyDelta: 0.03,
+    GDPCapita: 49240,
+    GDPCapitaMultiplier: 1.024,
     happiness: 7.0858500005,
     happinessDelta: -0.0376,
-    foodIndex: 0,
+    foodIndex: 9.52,
     financeIndex: 0,
     educationIndex: 0,
     techIndex: 0,
@@ -190,9 +201,11 @@ const initialContinents = (): ContinentSection[] => [
     birthRateDelta: -0.32,
     lifeExpectancy: 75.1,
     lifeExpectancyDelta: -0.02,
+    GDPCapita: 21363,
+    GDPCapitaMultiplier: 1.027,
     happiness: 6.304800034,
     happinessDelta: -0.053,
-    foodIndex: 0,
+    foodIndex: 90.6,
     financeIndex: 0,
     educationIndex: 0,
     techIndex: 0,
@@ -211,9 +224,11 @@ const initialContinents = (): ContinentSection[] => [
     birthRateDelta: -0.261,
     lifeExpectancy: 73.6,
     lifeExpectancyDelta: 0.37,
+    GDPCapita: 8560,
+    GDPCapitaMultiplier: 1.012,
     happiness: 6.163400173,
     happinessDelta: -0.021,
-    foodIndex: 0,
+    foodIndex: 91.2,
     financeIndex: 0,
     educationIndex: 0,
     techIndex: 0,
@@ -232,9 +247,11 @@ const initialContinents = (): ContinentSection[] => [
     birthRateDelta: -0.18,
     lifeExpectancy: 71.7,
     lifeExpectancyDelta: 0.13,
+    GDPCapita: 0,
+    GDPCapitaMultiplier: 0,
     happiness: 6.528499889,
     happinessDelta: -0.088,
-    foodIndex: 0,
+    foodIndex: 91.0,
     financeIndex: 0,
     educationIndex: 0,
     techIndex: 0,
@@ -253,9 +270,11 @@ const initialContinents = (): ContinentSection[] => [
     birthRateDelta: -0.24,
     lifeExpectancy: 83.4,
     lifeExpectancyDelta: 0.15,
+    GDPCapita: 53220,
+    GDPCapitaMultiplier: 1.027,
     happiness: 7.25210001,
     happinessDelta: -0.006,
-    foodIndex: 0,
+    foodIndex: 8.4, // Rough estimate; PNG has GHI 29.7 (very serious), others 5-10. Took average and subtracted from 100, then divided by 10.
     financeIndex: 0,
     educationIndex: 0,
     techIndex: 0,
@@ -274,9 +293,11 @@ const initialContinents = (): ContinentSection[] => [
     birthRateDelta: 0.16,
     lifeExpectancy: 71.5,
     lifeExpectancyDelta: 0.28,
+    GDPCapita: 30820,
+    GDPCapitaMultiplier: 1.025,
     happiness: 5.546000004,
     happinessDelta: -0.034,
-    foodIndex: 0,
+    foodIndex: 93.9,
     financeIndex: 0,
     educationIndex: 0,
     techIndex: 0,
@@ -297,6 +318,8 @@ const continentBase: ContinentSection = {
   birthRateDelta: 0,
   lifeExpectancy: 0,
   lifeExpectancyDelta: 0,
+  GDPCapita: 0,
+  GDPCapitaMultiplier: 0,
   happiness: 0,
   happinessDelta: 0,
   foodIndex: 0,
@@ -311,6 +334,12 @@ const continentBase: ContinentSection = {
   neighbors: [],
   xywh: [0, 0, 200, 200],
 }
+
+// export const initialContinentAverages: ContinentSection = initialContinents().reduce(
+//   (acc, cur) => ({
+
+//   }), continentBase
+// )
 
 export const initialMouseBuffer = (): MouseBuffer => ({
   lastMouseX: 0,
